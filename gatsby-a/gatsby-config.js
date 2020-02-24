1362528ae56f240b7bdca531870241a2195149d8
Andrei Kudrin <andreikudrin@gmail.com>
2020-02-24 21:47:22 +0000

    WebDev/Gatsby - First Project - GRAPHQL Intro

--- gatsby-a/gatsby-config.js
+++ gatsby-a/gatsby-config.js
@@ -6,5 +6,9 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Full Stack Bootcamp",
+    author: "A K",
+  },
   plugins: ["gatsby-plugin-sass"],
 }
--- gatsby-a/src/components/footer.js
+++ gatsby-a/src/components/footer.js
@@ -1,11 +1,22 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Footer = () => {
- return (
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
+
+  return (
     <footer>
-    <p>Created by ak, © 2020 </p>
+      <p>Created by {data.site.siteMetadata.author}, © 2020 </p>
     </footer>
- )
+  )
 }
 
 export default Footer
